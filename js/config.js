d3e8b7c3806deef8a721187697c5ef4621a6350a
ElyManzon <34364488+ElyManzon@users.noreply.github.com>
2022-05-17 11:24:47 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // SET VARIABLES FOR THIS Dashboard
 
-var config_Title = 'Typhoon Odette (Rai) Operation';
-var config_LastUpdate = '11 January 2022';
+var config_Title = 'Typhoon Agaton (Megi) Operation';
+var config_LastUpdate = '17 May 2022';
 
 var config_author = 'Elaine Manzon';
 var config_email = 'elaine.manzon@redcross.org.ph';
